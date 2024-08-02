dc69a8bf832ca27c8292721edcb90254600596ce
carolinekilian <carolinerosekilian@gmail.com>
2024-08-02 13:58:23 -0400

    Create disc2radmcHD110058.py

---
+++ disc2radmcHD110058.py
@@ -0,0 +1,8 @@
+import numpy as np
+import disc2radmc 
+import matplotlib.pyplot as plt
+import astropy.io.fits as pyfits
+
+#The following two lines aren't applicable for coding done in command line/running a vim file
+#%matplotlib inline
+#%config InlineBackend.figure_format='retina'
